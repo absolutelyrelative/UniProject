352e39c41dd272d75df4706aeddde4eac165d4f3
PaoloDanese <59060818+absolutelyrelative@users.noreply.github.com>
2020-12-21 23:00:14 +0100

    Debug

--- src/src/it/unisalento/pps/SimpleBooking/Testing/Test.java
+++ src/src/it/unisalento/pps/SimpleBooking/Testing/Test.java
@@ -7,6 +7,11 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
         Amministratore a = AmministratoreDAO.getInstance().findById(1);
-        System.out.print(a.getIdAmministratore() + '\n' + a.getId() + '\n' + a.getUtente_idUtente() + '\n' + a.getEmail() + '\n' + a.getPassword() + '\n' + a.getUsername());
+        System.out.println("IdUtente: " + a.getId());
+        System.out.println("\neMail: " + a.getEmail());
+        System.out.println("\nPassword: " + a.getPassword());
+        System.out.println("\nUsername: " + a.getUsername());
+        System.out.println("\nIdAmministratore: " + a.getIdAmministratore());
+        System.out.println("\nUtente_idUtente: " + a.getUtente_idUtente());
     }
 }
